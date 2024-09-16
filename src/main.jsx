71c2921db87d5b2bc31b3350ca008f1784f5d1db
williampetitpierre <williampp@hotmail.fr>
2024-09-16 10:32:24 +0200

    router disabled

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./routes/App.jsx";
 import "./index.css";
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from 'react-router-dom'
 import Root from "./routes/root.jsx";
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Root />
-    },
-    {
-        path: 'app',
-        element: <App />
-    }
-])
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <Root />
+//     },
+//     {
+//         path: 'app',
+//         element: <App />
+//     }
+// ])
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-      <RouterProvider router={router} />
+      <Root />
   );
